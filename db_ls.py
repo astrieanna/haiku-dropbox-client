f9679afeef9fe399a22048dad8cf6ab77077581f
Leah Hanson <astrieanna@gmail.com>
2013-01-23 22:06:59 -0500

    added initial python files: cli_client straight from dropbox SDK and the db_ls.py from the Hacker School interview.

---
+++ db_ls.py
@@ -0,0 +1,11 @@
+from cli_client import APP_KEY, APP_SECRET, DropboxTerm
+
+def main():
+    if APP_KEY == '' or APP_SECRET == '':
+        exit("You need to set your APP_KEY and APP_SECRET!")
+    term = DropboxTerm(APP_KEY, APP_SECRET)
+    #term.cmdloop()
+    term.do_ls()
+
+if __name__ == '__main__':
+    main()
