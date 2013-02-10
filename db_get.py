5cbe86de127a78666629b2a7381121bfe533e227
Leah Hanson <astrieanna@gmail.com>
2013-02-10 17:52:58 -0500

    added script for downloading files from dropbox. it takes two command line args -- src filepath on Dropbox and dest filepath locally.

---
+++ db_get.py
@@ -0,0 +1,15 @@
+import sys
+from cli_client import APP_KEY, APP_SECRET, DropboxTerm
+
+def main(src,dest):
+    if APP_KEY == '' or APP_SECRET == '':
+        exit("You need to set your APP_KEY and APP_SECRET!")
+    term = DropboxTerm(APP_KEY, APP_SECRET)
+
+    term.do_get(src,dest)
+
+if __name__ == '__main__':
+    #get cmdline args...
+    if len(sys.argv) != 3:
+        print "usage: python db_get.py <src> <dest>"
+    main(sys.argv[1],sys.argv[2])
