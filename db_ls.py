ad46708cbc2e29ceab9aa47245107c85a80862ef
Leah Hanson <astrieanna@gmail.com>
2013-02-11 08:29:13 -0500

    augmented ls script to understand how to cd to a new directory before running ls

--- db_ls.py
+++ db_ls.py
@@ -1,11 +1,19 @@
+import sys
 from cli_client import APP_KEY, APP_SECRET, DropboxTerm
 
-def main():
+def main(path):
     if APP_KEY == '' or APP_SECRET == '':
         exit("You need to set your APP_KEY and APP_SECRET!")
     term = DropboxTerm(APP_KEY, APP_SECRET)
-    #term.cmdloop()
+
+    if path != "":
+        term.do_cd(path)
     term.do_ls()
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        main("")
+    elif len(sys.argv) != 2:
+        print "usage: python db_ls.py <path>"
+    else:
+        main(sys.argv[1])
