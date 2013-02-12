ba7e98b7d5f642c43aab58068fc508c932dca957
Leah Hanson <Leah.A.Hanson@gmail.com>
2013-02-12 13:50:31 +0000

    Made Haiku Makefile, added header file, linked to Haiku API libraries.

---
+++ App.h
@@ -0,0 +1,17 @@
+#ifndef APP_H
+#define APP_H
+
+#include <Application.h>
+
+class App: public BApplication
+{
+public:
+  App(void);
+  void MessageReceived(BMessage *msg);
+private:
+  int32 fCount;
+  BWindow *myWindow;
+};
+
+#endif
+
--- HaikuDropbox.cpp
+++ HaikuDropbox.cpp
@@ -1,6 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "App.h"
+#include <Window.h>
+#include <Button.h>
+#include <View.h>
+#include <String.h>
+
+enum
+{
+  M_BUTTON_CLICKED = 'btcl'
+};
+
+App::App(void)
+  : BApplication("application/x-vnd.lh-MyDropboxClient")
+{
+  fCount = 0;
+
+  BRect frame(100,100,500,400);
+  myWindow = new BWindow(frame,"Dropbox FTW"
+                  , B_TITLED_WINDOW
+                  , B_ASYNCHRONOUS_CONTROLS
+                   | B_QUIT_ON_WINDOW_CLOSE);
+
+  BButton *button = new BButton(BRect(10,10,11,11),"button","Click me!"
+                                , new BMessage(M_BUTTON_CLICKED));
+  button->SetTarget(this);
+  button->ResizeToPreferred();
+  myWindow->AddChild(button);
+
+  myWindow->Show();
+}
+
+void
+App::MessageReceived(BMessage *msg)
+{
+  switch(msg->what)
+  {
+    case M_BUTTON_CLICKED:
+    {
+      fCount++;
+      BString labelString("Clicks:");
+      labelString << fCount;
+      myWindow->SetTitle(labelString.String());
+      break;
+    }
+    default:
+    {
+      BApplication::MessageReceived(msg);
+      break;
+    }
+  }
+}
+
+
 int
 run_script(char *cmd)
 {
@@ -15,7 +68,11 @@ run_script(char *cmd)
 }
 
 int
-main()
+main(void)
 {
+  App *app = new App();
+  app->Run();
+  delete app;
   run_script("python db_ls.py");
+  return 0;
 }
