eb7fa1a491ed27536ff032faa3fac340d80a38d1
Leah Hanson <Leah.A.Hanson@gmail.com>
2013-02-13 16:17:14 +0000

    added python script to remove a file from Dropbox (takes a dropbox path)

---
+++ db_rm.py
@@ -0,0 +1,14 @@
+import sys
+from cli_client import APP_KEY, APP_SECRET, DropboxTerm
+
+def main(path):
+    if APP_KEY == '' or APP_SECRET == '':
+        exit("You need to set your APP_KEY and APP_SECRET!")
+    term = DropboxTerm(APP_KEY, APP_SECRET)
+
+    term.do_rm(path)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "usage: python db_rm.py <dropbox path>"
+    main(sys.argv[1])
