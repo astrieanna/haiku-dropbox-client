cfcbc24b526430fd9979ba9d3d587ec082c72d1d
Leah Hanson <Leah.A.Hanson@gmail.com>
2013-02-13 13:03:24 +0000

    add a script for adding/sending a file to dropbox.

---
+++ db_put.py
@@ -0,0 +1,14 @@
+import sys
+from cli_client import APP_KEY, APP_SECRET, DropboxTerm
+
+def main(src,dest):
+    if APP_KEY == '' or APP_SECRET == '':
+        exit("You need to set your APP_KEY and APP_SECRET!")
+    term = DropboxTerm(APP_KEY, APP_SECRET)
+
+    term.do_put(src,dest)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print "usage: python db_put.py <local src> <dropbox dest>"
+    main(sys.argv[1],sys.argv[2])
