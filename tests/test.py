01414802f1909fcff7f87bb2d5288f4e26011f74
Leah Hanson <Leah.A.Hanson@gmail.com>
2013-03-29 22:06:22 +0000

    added comments to proto-test

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ from subprocess import Popen
 import time
 import os
 
+#setup
 os.system("rm -rf /boot/home/Dropbox/*")
 os.system("rm log.txt lines_*")
 os.system("touch log.txt")
@@ -10,23 +11,28 @@ os.system("touch log.txt")
 p = Popen(["../objects.x86-gcc2-release/hdbclient.exe"])
 time.sleep(2)
 
+#tell db_put what to say
 putlines = open("lines_db_put.txt",'w+')
 putlines.write("success")
 putlines.close()
 
+#create file
 foo = open("/boot/home/Dropbox/foo",'w+')
 foo.write("Hello,World")
 foo.close()
 
+#tell db_delta what to say
 deltalines = open("lines_db_delta.txt",'w+')
 deltalines.write("FILE /foo")
 deltalines.close()
 
+#wait for pull-deltas to finish
 time.sleep(10)
 
 # kill dbclient
 p.kill()
 
+# produce result
 print "Checking Assertions:"
 os.system("cat log.txt")
 
